125d02d5af158adca9664b7b2efc5a56ba2d5f5f
kanigreg <kanigreg@gmail.com>
2026-06-02 21:40:45 +0500

    feat: configure git-delta

--- modules/git.sh
+++ modules/git.sh
@@ -2,6 +2,14 @@
 # Модуль: git
 # Настройка git и его обвязки
 
+PACKAGES=(
+  git
+  git-delta
+)
+
+install_packages "${PACKAGES[@]}"
+
 local_home=$(get_real_home)
 
+link_config_dir "$HYDEENSE_DIR/configs/delta" "$local_home/.config/delta"
 link_config_dir "$HYDEENSE_DIR/configs/git" "$local_home/.config/git"
